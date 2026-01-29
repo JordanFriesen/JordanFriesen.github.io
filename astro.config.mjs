3feb0fa5ce39f2bf866298188325a470211c65bb
Jordan Friesen <river@Jordans-Mac-mini.local>
2026-01-29 09:51:53 -0800

    Configure for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://JordanFriesen.github.io',
+  base: '/astro-lenis-demo',
   integrations: [react()],
 
   vite: {
